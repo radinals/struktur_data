5303725a5cea983a278e8ed7587dc69670ddfb99
radinals <radinalsaragih@gmail.com>
2024-02-07 19:45:01 +0700

    rewrote most of the implementation

--- linked_list/Singly/Singly.cpp
+++ linked_list/Singly/Singly.cpp
@@ -1,56 +1,72 @@
-#include <cstddef>
 #include <iostream>
-#include <iterator>
 #include <string>
 
 typedef struct Node {
-	std::string text;
+	std::string str;
 	Node* next;
 } Node;
 
-void push(Node* head, std::string text);
-void printnodes(Node* head);
+void add_node(Node*, const std::string&);
+void print_all(Node* head);
+void delete_nodes(Node* head);
 
 int
 main()
 {
 	Node* head = new Node;
-	head->next = nullptr;
-	std::size_t count = 0;
-	std::string text;
 
-	for (int i = 0; i < 3; i++) {
-		std::cout << "INPUT: ";
-		getline(std::cin, text);
-		if (count <= 0)
-			head->text = text;
-		else
-			push(head, text);
-		count++;
-	}
+	add_node(head, "hello");
+	add_node(head, "World");
 
-	printnodes(head);
+	print_all(head);
+	delete_nodes(head);
 
 	return 0;
 }
 
 void
-push(Node* head, std::string text)
+add_node(Node* head, const std::string& str)
 {
-	Node* current = head;
-	while (current->next != nullptr) current = current->next;
-	current->next = new Node;
-	current->next->text = text;
-	current->next->next = nullptr;
+
+	if (head->str.empty()) { // if the head is not set
+		head->str = str;
+		head->next = nullptr;
+	} else { // new nodes
+		Node* nw_node;
+		Node* current = head;
+		// find the last node
+		while (current->next != nullptr) current = current->next;
+
+		// create the node
+		nw_node = new Node;
+		nw_node->str = str;
+		nw_node->next = nullptr;
+
+		// add the node to the linked list
+		current->next = nw_node;
+	}
 }
 
 void
-printnodes(Node* head)
+print_all(Node* head)
 {
 	Node* current = head;
-
+	// print all the data in the node list
 	while (current != nullptr) {
-		std::cout << current->text << "\n";
+		std::cout << current->str;
 		current = current->next;
-	}
+	};
+}
+
+void
+delete_nodes(Node* head)
+{
+	Node* tmp = head;
+	// delete the linked list nodes,
+	// starting from the head.
+	while (head != nullptr) {
+		tmp = head;
+		head = head->next;
+		delete tmp;
+	};
 }
