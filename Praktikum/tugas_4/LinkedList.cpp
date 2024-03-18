5489e8362c6ef82eebaae96229cdf9cf70edbd56
radinals <radinalsaragih@gmail.com>
2024-03-19 00:12:07 +0700

    added tugas 4

---
+++ Praktikum/tugas_4/LinkedList.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+struct Node {
+	std::string nama;
+	Node* next;
+
+	Node(const std::string& nama)
+	{
+		this->nama = nama;
+		next = NULL;
+	}
+};
+
+struct LinkedList {
+	Node* Head = NULL;
+
+	void insert(const std::string& nama);
+	void append(const std::string& nama);
+	void print();
+};
+
+void
+LinkedList::insert(const std::string& nama)
+{
+	Node* new_node = new Node(nama);
+
+	if (Head == NULL) {
+		Head = new_node;
+	} else {
+		Node* old_head = Head;
+
+		Head = new_node;
+		Head->next = old_head;
+	}
+}
+
+void
+LinkedList::append(const std::string& nama)
+{
+	Node* new_node = new Node(nama);
+
+	if (Head == NULL) {
+		Head = new_node;
+	} else {
+		Node* h = Head;
+		while (h->next != NULL) h = h->next;
+		h->next = new_node;
+	}
+}
+
+void
+LinkedList::print()
+{
+	Node* h = Head;
+	while (h != NULL) {
+		std::cout << "-> " << h->nama << "\n";
+		h = h->next;
+	};
+}
+
+int
+main()
+{
+	LinkedList list;
+
+	list.insert("Sigit");
+	list.append("Gogon");
+	list.insert("Alipia");
+	list.append("Wardah");
+
+	list.print();
+
+	return 0;
+}
